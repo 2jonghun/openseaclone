9cf9244c68be3a6ac82de6529f128038b63149dc
jonghun <ghost9409@gmail.com>
2022-06-30 15:53:11 +0900

    add resize event

---
+++ README.md
@@ -0,0 +1,2 @@
+# opensea clone
+just navbar
--- index.js
+++ index.js
@@ -1,17 +1,71 @@
 const inputBox = document.querySelector('.search-input');
-const searchBtn = document.querySelector('.navbar__search-button');
-
-inputBox.addEventListener('focus', () => {
-  searchBtn.style.display = 'block';
-})
+const searchBtn = document.querySelector('.navbar__search-xbutton');
+const inputBox2 = document.querySelector('.search-input2');
+const searchBtn2 = document.querySelector('.navbar__search-xbutton2');
 
 searchBtn.addEventListener('click', () => {
   if (inputBox.value) {
     inputBox.value = '';
     inputBox.focus();
+    searchBtn.style.display = 'none';
   }
 })
 
-inputBox.addEventListener('blur', () => {
-  searchBtn.style.display = 'none';
+inputBox.addEventListener('keyup', () => {
+  if (inputBox.value) {
+    searchBtn.style.display = 'block';
+  } else {
+    searchBtn.style.display = 'none';
+  }
 })
+
+searchBtn2.addEventListener('click', () => {
+  if (inputBox2.value) {
+    inputBox2.value = '';
+    inputBox2.focus();
+    searchBtn2.style.display = 'none';
+  }
+})
+
+inputBox2.addEventListener('keyup', () => {
+  if (inputBox2.value) {
+    searchBtn2.style.display = 'block';
+  } else {
+    searchBtn2.style.display = 'none';
+  }
+})
+
+const showInputBtn = document.querySelector('.navbar__search-icon-box2-button');
+const navbarLogo = document.querySelector('.navbar__logo');
+const navbarToogle = document.querySelector('.navbar__toogle');
+const targetInputbox = document.querySelector('.navbar__input-box');
+const angleBtn = document.querySelector('.angle-btn');
+
+showInputBtn.addEventListener('click', () => {
+  navbarLogo.style.display = 'none';
+  navbarToogle.style.display = 'none';
+  showInputBtn.style.display = 'none';
+  targetInputbox.style.display = 'block'
+})
+
+angleBtn.addEventListener('click', () => {
+  targetInputbox.style.display = 'none';
+  navbarLogo.style.display = 'flex';
+  navbarToogle.style.display = 'block';
+  showInputBtn.style.display = 'block';
+})
+
+const searchInput1 = document.querySelector('.navbar__input-box1');
+
+window.addEventListener('resize', () => {
+  const browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  if (browserWidth >= 600) {
+    navbarLogo.style.display = 'flex';
+    navbarToogle.style.display = 'block';
+    showInputBtn.style.display = 'block';
+    if (targetInputbox.style.display == 'block') {
+      targetInputbox.style.display = 'none'
+    }
+  }
+});
